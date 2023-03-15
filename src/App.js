2d5a4798e6cf8fb3a32417ee2aeff145244cd2fd
Himanshu <himanshuyadav201000@gmail.com>
2023-03-15 12:09:30 +0530

    React-4 assignment

--- src/App.js
+++ src/App.js
@@ -1,81 +1,31 @@
-import React from "react";
-import { useState } from "react";
-import "./App.css"
+import CardProfile from './CardProfile';
+import './CardProfile.css'
+// import './App.css'
 
-export default function App() {
-
-    //Answer 1
-     const[todo,setTodo]=useState("");
-    const[arr,setArr]=useState(["Do Some Work Now!!"])
-
-function handleChange(e) {
-setTodo(e.target.value);
-}
-
-function handleText(){
-    setArr([...arr,todo]);
-}
-
-// //answer 2
-
-const[count,setCount]=useState(0);
-function handleIncrease(){
-    setCount(count+1);
-}
-function handleDecrease(){
-    setCount(count-1 );
-}
-
-// //answer3
-const[target,setTarget]=useState(true)
-
-function handleName(){
-    setTarget(!target);
-}
-
-return(
-    <div id="Content">
-        <h4>On clicking Add Button will show in below task list</h4>
-         <input value={todo} type="text" onChange={handleChange}/>
-        <button onClick={handleText}>Add</button>
-        <ul>
-            {
-                arr.map((x)=>{
-                    return <p>{x}</p>
-                })
-            }
-        </ul>
-        <br></br><br></br>
-
-        {/* answer 2 */}
-        <h1>{count}</h1>
-        <button onClick={handleIncrease} >Increase</button>
-        <button onClick={handleDecrease}>Decrease</button>
-
-
-
-
-
-
-        <br></br><br></br>
-        {/* answer 3 */}
-        {target? <p>My Name Is Amit</p> : <p>My Name is Rajan</p> }
-        <button onClick={handleName}>Change Name</button>
-    </div>
-);
-
-
-
-//import { useState } from 'react';//
-function LogButtonClicks() {
-  const [count, setCount] = useState(0);
+function App() {
+  const User1 = {
+    name : 'Amit' ,
+    image : 'https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg' ,
+    designation : 'Graphic Designer ',
+    description : 'Highly creative and multitalented Graphic Designer with extensive experience in multimedia, marketing, and print design.' 
+  }
+ 
+  const User2 = {
+    name : 'Ruhi' ,
+    image : 'https://images.pexels.com/photos/3775131/pexels-photo-3775131.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' ,
+    description : 'perform a variety of music for recordings and live audiences. They audition for positions in choruses, orchestras, bands, plays, 				and other types of music groups.' ,
+    designation : 'Singer'
+  }
   
-  const handle = () => {
-    const updatedCount = count + 1;
-    console.log(`Clicked ${updatedCount} times`);
-    setCount(updatedCount);
-  };
-  console.log('I rendered!');
-  return <button onClick={handle}>Click me</button>;
+   return (
+    <div className="App">
+      <header className='App-header'>
+      <CardProfile user = {User1} />
+      <br></br>
+      <CardProfile user = {User2} />
+      </header>
+    </div>
+   )
 }
-}
+
+export default App;
